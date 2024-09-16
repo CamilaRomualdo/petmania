e81055ac0e3ba28f012696916a86f14e0066a69c
Camila Romualdo <camilaevelynromualdo@gmail.com>
2024-09-16 14:56:48 -0300

    Initial Commit

---
+++ src/test/java/com/camilaromualdo/petmania/PetmaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilaromualdo.petmania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
